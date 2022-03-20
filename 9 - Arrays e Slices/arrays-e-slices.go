167f521a115df3181bb5abb858fe02acc7fb7d7a
Victor Andrade <victor.andrade@alfaerp.com.br>
2022-03-20 18:05:41 -0300

    ate aula de slices

---
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("Arrays e Slices")
+
+	var array1 [5]string
+	array1[0] = "Posicao 0"
+	fmt.Println(array1)
+
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	slice := []int{10, 11, 12, 13, 14, 15, 16, 17}
+	fmt.Println(slice)
+
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(array3))
+
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Posição Alterada"
+	fmt.Println(array2)
+
+	//Arrays internos
+	// quando o slice estoura a capacidade ele cria outro array interno com o dobro do tamanho
+	slice3 := make([]float32, 10, 11)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // tamanho
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	// Aqui vamos perceber que ele dobrou o tamanho do array interno
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // tamanho
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4)) // tamanho
+	fmt.Println(cap(slice4)) // capacidade
+
+	slice4 = append(slice4, 6)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4)) // tamanho
+	fmt.Println(cap(slice4)) // capacidade
+
+}
